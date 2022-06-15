9842013ff690d1d7283af3b29a05d30f63e434b2
yuvinrho <yvrho@naver.com>
2022-06-15 21:11:52 +0900

    Time: 2082 ms (5.05%), Space: 18.2 MB (38.95%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.swift
+++ 217-contains-duplicate/217-contains-duplicate.swift
@@ -1,6 +1,6 @@
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
-        for num in nums {
+        for num in Set(nums) {
             if nums.filter { $0 == num }.count >= 2 { return true }
         }
         return false
