17fcd92728a271f8b3d7a36663207996e9235b94
yuvinrho <yvrho@naver.com>
2022-06-22 22:36:00 +0900

    Time: 13 ms (41.26%), Space: 14 MB (68.49%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.swift
+++ 88-merge-sorted-array/88-merge-sorted-array.swift
@@ -1,5 +1,5 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        nums1 = ((0..<m).map { nums1[$0] } + nums2).sorted()
+        nums1 = (nums1[0..<m] + nums2).sorted()
     }
 }
