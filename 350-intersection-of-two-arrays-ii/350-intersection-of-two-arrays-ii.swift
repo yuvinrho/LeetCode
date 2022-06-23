5efca330cf61b04f24e7561058332a8277d2c52d
yuvinrho <yvrho@naver.com>
2022-06-23 14:15:19 +0900

    Time: 24 ms (48.22%), Space: 14.1 MB (66.22%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.swift
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.swift
@@ -1,12 +1,7 @@
 class Solution {
     func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
         var result: [Int] = []
-        var nums1 = nums1
-        var nums2 = nums2
-        if nums1.count < nums2.count {
-            swap(&nums1, &nums2)
-        }
-        
+        var nums1 = nums1        
         for number in nums2 {
             if nums1.contains(number) {
                 result.append(number)
