66d21978e8370e016896ac12d2349ebed375761b
yuvinrho <yvrho@naver.com>
2022-06-24 17:02:41 +0900

    Time: 27 ms (54.55%), Space: 14.1 MB (74.75%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
@@ -1,7 +1,6 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
         for row in 0..<matrix.count {
-            print(matrix[row].last!)
             if matrix[row].last! >= target {
                 return matrix[row].contains(target)
             } 
