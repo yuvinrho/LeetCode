926224bd74b10db0916a7a63d0a1bdef77b21892
yuvinrho <yvrho@naver.com>
2022-06-26 17:15:06 +0900

    Time: 27 ms (7.07%), Space: 13.7 MB (97.64%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.swift
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.swift
@@ -12,13 +12,16 @@ class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
         guard let list1 = list1 else { return list2 }
         guard let list2 = list2 else { return list1 }
+        var mergedList: ListNode? = nil
         
         if list1.val <= list2.val {
-            list1.next = mergeTwoLists(list1.next, list2)
-            return list1
+            mergedList = list1
+            mergedList!.next = mergeTwoLists(list1.next, list2)
+        } else {
+            mergedList = list2
+            mergedList!.next = mergeTwoLists(list1, list2.next)
         }
         
-        list2.next = mergeTwoLists(list1, list2.next)
-        return list2
+        return mergedList
     }
 }
